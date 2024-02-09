cbf09ecb74f0eaabd546dee5daef1de69738a3ab
Daniel <daanf@live.nl>
2024-02-09 13:46:44 +0100

    update code

--- proper-code.js
+++ proper-code.js
@@ -1,41 +1,42 @@
-console.log(
-    "Hi guys, hopefully you don't mind me leaving a little something for you in DEVELOPMENT mode :)"
-);
+console.log("Hi team, I've left a little surprise for you in DEVELOPMENT mode :)");
 
-const kekw = "https://emoji.slack-edge.com/T028LR0EWCS/kek/af5511f0b05a8a4b.jpg";
-const mufasa = "https://emoji.slack-edge.com/T028LR0EWCS/mufasa/69041d6a7cd200d4.gif";
+const img1URL = "https://emoji.slack-edge.com/T028LR0EWCS/kek/af5511f0b05a8a4b.jpg";
+const img2URL = "https://emoji.slack-edge.com/T028LR0EWCS/mufasa/69041d6a7cd200d4.gif";
 
-const kekwImg = `<img id="kekw" class="super-happy-funtime" src="${kekw}" alt="KEKW" style="width: 50px; height: 50px; display: none;"/>`;
-const mufasaImg = `<img id="mufasa" class="super-happy-funtime" src="${mufasa}" alt="Mufasa" style="width: 50px; height: 50px; display: none;"/>`;
+const img1HTML = `<img id="img1" class="dynamic-img" src="${img1URL}" alt="Img1" style="width: 50px; height: 50px; display: none;"/>`;
+const img2HTML = `<img id="img2" class="dynamic-img" src="${img2URL}" alt="Img2" style="width: 50px; height: 50px; display: none;"/>`;
 
 const styles = `
-    .super-happy-funtime {
+    .dynamic-img {
         position: fixed;
         z-index: 999999;
     }
 `;
 
-// Append styles to the document head
 const styleSheet = document.createElement("style");
 styleSheet.type = "text/css";
 styleSheet.innerText = styles;
 document.head.appendChild(styleSheet);
 
-// Append images to the document body
-document.body.innerHTML += kekwImg + mufasaImg;
-
 let screensaverActive = false;
 let screensaverTimer;
 
-function bounceAnimation(elementId, velocityX, velocityY) {
-    const img = document.getElementById(elementId);
-    let posX = 0,
-        posY = 0;
-    img.style.display = "block";
+function createImageElement(htmlStr) {
+    const div = document.createElement('div');
+    div.innerHTML = htmlStr.trim();
+    return div.firstChild; 
+}
+
+const img1 = createImageElement(img1HTML);
+const img2 = createImageElement(img2HTML);
+
+function bounceAnimation(img, velocityX, velocityY) {
+    let posX = 0, posY = 0;
+    img.style.display = 'block';
 
     function update() {
         if (!screensaverActive) {
-            img.style.display = "none";
+            img.remove();
             return;
         }
 
@@ -50,8 +51,8 @@ function bounceAnimation(elementId, velocityX, velocityY) {
             velocityY *= -1;
         }
 
-        img.style.left = posX + "px";
-        img.style.top = posY + "px";
+        img.style.left = posX + 'px';
+        img.style.top = posY + 'px';
 
         requestAnimationFrame(update);
     }
@@ -61,8 +62,10 @@ function bounceAnimation(elementId, velocityX, velocityY) {
 
 function startScreensaver() {
     screensaverActive = true;
-    bounceAnimation("kekw", 2, 3);
-    bounceAnimation("mufasa", 3, 2);
+    document.body.appendChild(img1);
+    document.body.appendChild(img2);
+    bounceAnimation(img1, 2, 3);
+    bounceAnimation(img2, 3, 2);
 }
 
 function stopScreensaver() {
@@ -77,10 +80,8 @@ function resetScreensaverTimer() {
     }
 }
 
-// Add event listeners for user interactions
-["mousemove", "click", "keypress"].forEach((event) => {
+['mousemove', 'click', 'keypress'].forEach(event => {
     window.addEventListener(event, resetScreensaverTimer);
 });
 
-// Initialize the screensaver timer
 resetScreensaverTimer();
